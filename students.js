1c438cc94324b0cead1cb3b088b79ea91acd90e8
BNBCE <beraberiacsc@gmail.com>
2025-07-07 14:29:13 +0530

    Add files via upload

---
+++ students.js
@@ -0,0 +1,5 @@
+const students = [
+  { id: "rahul", password: "123", name: "Rahul Das", course: "DCA", batch: "10:00 AM" },
+  { id: "soma", password: "456", name: "Soma Roy", course: "ADCA", batch: "2:00 PM" },
+  { id: "imran", password: "789", name: "Imran Khan", course: "Tally", batch: "4:00 PM" }
+];
